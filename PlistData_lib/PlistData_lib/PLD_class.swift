e4861f5d7df201d26c8755cfc8ec03be86e6d6a3
laurent aubourg <laurent.aubourg@free.fr>
2021-04-10 18:15:04 +0200

    ajout de méthodes : get_value, set_value

--- PlistData_lib/PlistData_lib/PLD_class.swift
+++ PlistData_lib/PlistData_lib/PLD_class.swift
@@ -9,22 +9,53 @@ import Foundation
 
 public class PLD_class: PlistDataProtocol {
   public  var _plistManager: PlistManager
+  
+    //MARK: Methods
     
    public  init (fileName:String){
     _plistManager = PlistManager(name: fileName)
+    }
+
+   public  func newVar(name:String,value:Any){
+        dynamicData[name] = value as AnyObject
+    }
+    public func getValueof(p_var:String)-> Any?{
+        
+         guard (dynamicData[p_var] != nil)else{
+             return nil
+        }
+        return dynamicData[p_var]
+ 
+    }
+    public func getValueof(p_let:String)-> Any?{
+        
+         guard (data[p_let] != nil)else{
+             return nil
+        }
+        return data[p_let]
+ 
+    }
+   public func setValueof(_ p_var:String,_ value:AnyObject){
+        
+         guard (dynamicData[p_var] != nil)else{
+             return
+        }
+        dynamicData[p_var] = value
+ 
     }
     public  func saveData(key:String ,data: Dictionary<String, AnyObject>){
          
          _plistManager.saveValue(value:data as AnyObject, forKey:key)
      }
+  
+   
+    // MARK: les variables
     public  var data:Dictionary<String, AnyObject>{
          get{
             return  _plistManager.getValueForKey(key:"data") as! Dictionary<String, AnyObject>
          }
         
      }
-    // MARK: les variables
-    
      public var dynamicData:Dictionary<String, AnyObject>{
          get{
              return _plistManager.getValueForKey(key:"dynamicData") as! Dictionary<String, AnyObject>
